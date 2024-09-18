2dc42c58ae68cecc4b0435963cf1c6efcaa00254
Simon Fredriksson <simon@lan2k.org>
2024-09-19 01:26:41 +0200

    minor edits
    - add methods init, auth, unlock to http api.

--- apiserver.js
+++ apiserver.js
@@ -14,25 +14,33 @@ aptus.debug = true;
 const app = express();
 app.use(morgan("combined"));
 
+app.get("/init", async (req, res) => {
+    const action = await aptus.initialize();
+    res.send(action);
+});
+
+app.get("/auth", async (req, res) => {
+    const action = await aptus.authenticate();
+    res.send(action);
+});
+
 app.get("/list", async (req, res) => {
-    //const auth = await aptus.authenticate();
-    //if (auth) {
-    const doors = await aptus.listDoors();
-    res.send(doors);
-    //} else {
-    //res.send(auth);
-    //}
+    const action = await aptus.listDoors();
+    res.send(action);
+});
+
+app.get("/unlock/:doorId", async (req, res) => {
+    const doorId = req.params.doorId;
+    const action = await aptus.unlockDoor(doorId);
+    res.send(action);
 });
 
 app.get("/open/:doorId", async (req, res) => {
+    await aptus.initialize();
+    await aptus.authenticate();
     const doorId = req.params.doorId;
-    //const auth = await aptus.authenticate();
-    //if (auth) {
-    const unlock = await aptus.unlockDoor(doorId);
-    res.send(unlock);
-    //} else {
-    //res.send(auth);
-    //}
+    const action = await aptus.unlockDoor(doorId);
+    res.send(action);
 });
 
 app.listen(listenport, async () => {
